82e7ceec49884894ccd2f66003432a7baa13b89f
Agim Aljanata <2012220088@unmuhpnk.ac.id>
2023-07-23 00:17:05 +0700

    menambahkan beberapa gambar dan deskripsi

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -44,7 +44,7 @@
                             
                             </svg><svg class="shape-2 d-none d-sm-block" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg"></svg>
                         <div class="device-wrapper">
-                        <img src="/img/Desain produk.png" style = "width : 350px ; height:450px">
+                        <img src="/img/Desain produk.png" style = "width : 350px ; height:480px">
             </div>
         </div>
     </section>
@@ -53,21 +53,22 @@
         <div class="container px-5">
             <div class="row gx-5 align-items-center justify-content-center justify-content-lg-between">
                 <div class="col-12 col-lg-5">
-                    <h2 class="display-4 lh-1 mb-4">Enter a new age of web design</h2>
-                    <p class="lead fw-normal text-muted mb-5 mb-lg-0">This section is perfect for featuring some information about your application, why it was built, the problem it solves, or anything else! There's plenty of space for text here, so don't worry about writing too much.</p>
+                    <h2 class="display-4 lh-1 mb-4">MENGAPA HARUS SUPER LAUNDRY?</h2>
+                    <p class="lead fw-normal text-muted mb-5 mb-lg-0"> Karena dapat menghemat waktu, mendapatkan hasil cucian yang bersih dan wangi, menjaga kualitas dan keawetan pakaian, menjaga kesehatan, serta harga yang sangat terjangkau.</p>
                 </div>
                 <div class="col-sm-8 col-md-6">
-                    <div class="px-5 px-sm-0"><img class="img-fluid rounded-circle" src="https://source.unsplash.com/u8Jn2rzYIps/900x900" alt="..." /></div>
+                    <div class="px-5 px-sm-0">
+                        <img src="/img/mesincuciproduk1.jpg" alt="..." style = "width :500px ; height: 500px/>
+                    </div>
                 </div>
             </div>
         </div>
     </section>
     <section class="bg-gradient-primary-to-secondary" id="download">
         <div class="container px-5">
-            <h2 class="text-center text-white font-alt mb-4">Get the app now!</h2>
+            <h2 class="text-center text-white font-alt mb-4">Nikmati Layanan Super Laundry!</h2>
             <div class="d-flex flex-column flex-lg-row align-items-center justify-content-center">
-                <a class="me-lg-3 mb-4 mb-lg-0" href="#!"><img class="app-badge" src="assets/img/google-play-badge.svg" alt="..." /></a>
-                <a href="#!"><img class="app-badge" src="assets/img/app-store-badge.svg" alt="..." /></a>
+            
             </div>
         </div>
     </section>
