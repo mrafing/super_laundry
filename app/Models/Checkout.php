b91ab5d6f71e506dc6142deeae5308499ab23c65
mrafing <dwpu110300@gmail.com>
2023-07-23 19:36:33 +0700

    memperbaiki table checkout dan modal checkout

--- app/Models/Checkout.php
+++ app/Models/Checkout.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Checkout extends Model
 {
     use HasFactory;
+
+    protected $guarded = ['id'];
+
+    public function user () {
+        return $this->belongsTo(User::class);
+    }
 }
--- database/migrations/2023_07_21_103140_create_checkouts_table.php
+++ database/migrations/2023_07_21_103140_create_checkouts_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('checkouts', function (Blueprint $table) {
             $table->id();
             $table->foreignId('id_user');
-            $table->foreignId('id_chart');
             $table->integer('total');
             $table->timestamps();
         });
