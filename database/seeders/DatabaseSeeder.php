495b2f5af1b86d164fabbefdde2280cf112afd69
Agim Aljanata <2012220088@unmuhpnk.ac.id>
2023-07-26 12:46:25 +0700

    menambahkan seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Database\Seeders;
+use App\Models\Product;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -18,5 +19,34 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        Product::create([
+            'id' => '1',
+            'name' =>'Baju',
+            'kg' => '5',
+            'price' => '30000',
+            'image' => 'baju.png'
+        ]);
+        Product::create([
+            'id' => '2',
+            'name' =>'Sepatu',
+            'kg' => '1',
+            'price' => '30000',
+            'image' => 'sepatu.png'
+        ]);
+        Product::create([
+            'id' => '3',
+            'name' =>'Bed Cover',
+            'kg' => '5',
+            'price' => '50000',
+            'image' => 'bedcover.png'
+        ]);
+        Product::create([
+            'id' => '4',
+            'name' =>'Selimut',
+            'kg' => '2',
+            'price' => '25000',
+            'image' => 'selimut.png'
+        ]);
     }
 }
